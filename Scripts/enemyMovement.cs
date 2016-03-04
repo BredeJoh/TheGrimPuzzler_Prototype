a9348a83653ca17a61ed9612c2a4d071e0146f7e
BredeJoh <brede_96@hotmail.com>
2016-03-04 12:03:06 +0100

    enemyMovement

---
+++ Scripts/enemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMovement : MonoBehaviour {
+
+    Rigidbody2D body2D;
+    float speed = 7.0f;
+    private bool moveRight = true;
+
+	// Use this for initialization
+	void Start () {
+        body2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (moveRight)
+        {
+            body2D.velocity = new Vector2(speed, body2D.velocity.y);
+            transform.localScale = new Vector2(-1f, 1f);
+            
+        }
+        else if (moveRight == false)
+        {
+            body2D.velocity = new Vector2(-speed, body2D.velocity.y);
+            transform.localScale = new Vector2(1f, 1f);
+        }
+}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "movementTrigger")
+        {
+            if (moveRight) {
+                moveRight = false;
+            }
+            else
+            {
+                moveRight = true;
+            }
+        }
+    }
+}
