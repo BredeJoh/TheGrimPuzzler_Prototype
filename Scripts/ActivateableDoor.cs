3a1d884d28a857482aba0a6f128d9981666e1581
MartinLien <m.lsk95@hotmail.com>
2016-03-08 14:11:00 +0100

    fiksa på PressurePlate

--- Scripts/ActivateableDoor.cs
+++ Scripts/ActivateableDoor.cs
@@ -5,18 +5,33 @@ public class ActivateableDoor : MonoBehaviour {
 
 	GameObject door;
 
+	bool activated = true;
+
 	void Start (){
 		door = GameObject.FindGameObjectWithTag ("door");
 	}
 
-	void OnTriggerEnter2D (Collider2D other){
+	void OnTriggerStay2D (Collider2D other){
 		if (other.gameObject.tag == "brute"){
 			//Destroy (door);
-			door.GetComponent<SpriteRenderer>().enabled = !enabled;
-			door.GetComponent<BoxCollider2D>().enabled = !enabled;
+			//door.GetComponent<SpriteRenderer>().enabled = !enabled;
+			//door.GetComponent<BoxCollider2D>().enabled = !enabled;
+			if (activated){
+			StartCoroutine (PushDownPlate());
+				activated = false;
+			}
 			//gameObject.GetComponent<BoxCollider2D> ().enabled = !gameObject.GetComponent<BoxCollider2D> ().enabled;
 		}
 	}
 
+	IEnumerator PushDownPlate (){
 
+		yield return new WaitForSeconds (1);
+		door.GetComponent<SpriteRenderer>().enabled = !enabled;
+		door.GetComponent<BoxCollider2D>().enabled = !enabled;
+		gameObject.GetComponent<EdgeCollider2D> ().enabled = !enabled;
+		transform.position = new Vector2 (transform.position.x, transform.position.y-1);
+
+
+	}
 }
